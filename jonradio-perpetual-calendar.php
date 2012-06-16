2ccdc704eafbed9b4101d7dd7dbf8196b2a646c3
adiant <adiant@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-16 16:05:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jonradio-perpetual-calendar/trunk@559074 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jonradio-perpetual-calendar.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: jonradio Perpetual Calendar
+Plugin URI: http://jonradio.com/plugins/jonradio-perpetual-calendar/
+Description: Your choice of Shortcode or php function to return a message indicating the full name of the day of the week for any given date, the typical usage of a so-called Perpetual Calendar. 
+Version: 0.9
+Author: jonradio
+Author URI: http://jonradio.com/plugins
+License: GPLv2
+*/
+
+/*  Copyright 2012  jonradio  (email : info@jonradio.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( 'init', 'jr_pc_init' );
+
+function jr_pc_init() {
+	if ( !function_exists( 'jr_weekday' ) ) {
+		/**
+		* Weekday message function
+		* 
+		* Returns a message listing the date given and the full name of the weekday for that given date, or an error message.
+		*
+		* @param    int     $year       Year portion of date
+		* @param    int     $month      Month portion of date
+		* @param    int     $day        Day of Month portion of date
+		* @return   string              Message listing date and weekday, or error message.
+		*/
+		function jr_weekday( $year, $month, $day ) {
+			if ( !isset( $year ) || !isset( $month ) || !isset( $day ) ) {
+				return 'Error:  one or more of the three jr_weekday parameters (year, month, day) were either not specified or the variable specified is unassigned.';
+			}
+			if ( !jr_pc_signed_integer( $year ) || !jr_pc_signed_integer( $month ) || !jr_pc_signed_integer( $day ) ) {
+				return 'Error:  one or more of the three jr_weekday parameters (year, month, day) is not an integer.';
+			}		
+			$jd = gregoriantojd( $month, $day, $year );
+			if ( $jd == 0 ) {
+				return 'Error:  earliest allowed date is November 25, 4714 B.C.';
+			} else {
+				if ( jdtogregorian( $jd ) == "$month/$day/$year" ) {
+					return jdmonthname( $jd, 1 ) . " $day, " . abs( $year ) . ' ' . jr_pc_century( $year ) . ' is a ' . jddayofweek( $jd, 1 );
+				} else {
+					return 'Error:  date specified does not exist.';
+				}
+			}
+		}
+	}
+	
+	add_shortcode( 'pcal', 'jr_pc_pcal' );
+}
+
+/**
+ * [pcal] Shortcode
+ * 
+ * Returns an HTML Form in which a user may enter a date.
+ * If a Date was just entered, displays the Day of Week or an error message.
+ *
+ * @return   string              HTML to display Date entry form and the Day of Week for a previous inquiry
+ */
+function jr_pc_pcal() {
+	$min = -4714;
+	$max = 9999;
+	
+	//	Default Date:
+	$year = 1962;
+	$month = 2;
+	$day = 9;
+	
+	$output = '<hr />';
+	if ( isset( $_POST['jrsubmit'] ) ) {
+		$century = $_POST['jrcentury'];
+		if ( $century < 0 ) {
+			$sign = -1;
+		} else {
+			$sign = 1;
+		}
+		$year = $sign * ( ( floor( abs( $century ) / 100 ) * 100 ) + $_POST['jrten'] + $_POST['jryear'] );
+		$month = $_POST['jrmonth'];
+		$day = $_POST['jrday'];
+		$output .= '<p>' . jr_weekday( $year, $month, $day ) . '</p>';
+		$output .= '<hr />';
+	}
+	
+	$output .= '<form method=post> <button type="submit" name="jrsubmit" value="jrsubmit">Display Day of Week</button> <select name="jrmonth">';
+	for ( $i = 1; $i <= 12; $i++ ) {
+		if ( $i == $month ) {
+			$selected = 'selected="selected"';
+		} else {
+			$selected = '';
+		}
+		$output .= "<option $selected value=$i>" . date( 'F', mktime( 0, 0, 0, $i, 1, 2001 ) ) . '</option>';
+	}
+	$output .= '</select> <select name="jrday">';
+	for ( $i = 1; $i <= 31; $i++ ) {
+		if ( $i == $day ) {
+			$selected = 'selected="selected"';
+		} else {
+			$selected = '';
+		}		
+		$output .= "<option $selected>$i</option>";
+	}
+	
+	if ( $year < 0 ) {
+		$sign = -1;
+	} else {
+		$sign = 1;
+	}	
+	
+	$output .= '</select>, <select name="jrcentury">';
+	for ( $i = $min; $i <= $max; $i += 100 ) {
+		$century = floor( abs( $i ) / 100 );
+		if ( intval( $i / 100 ) == intval( $year / 100 ) ) {
+			$selected = 'selected="selected"';
+		} else {
+			$selected = '';
+		}
+		$output .= "<option $selected value=$i>" . jr_pc_century( $i ) . " $century</option>";
+	}
+	$output .= '</select> <select name="jrten">';
+	for ( $i = 0; $i <= 90; $i += 10 ) {
+		if ( $i / 10 == floor( abs( $year ) / 10 ) % 10 ) {
+			$selected = 'selected="selected"';
+		} else {
+			$selected = '';
+		}	
+		$output .= "<option $selected value=$i>" . $i / 10 . '</option>';
+	}
+	$output .= '</select> <select name="jryear">';
+	for ( $i = 0; $i <= 9; $i++ ) {
+		if ( $i == abs( $year ) % 10 ) {
+			$selected = 'selected="selected"';
+		} else {
+			$selected = '';
+		}
+		$output .= "<option $selected>$i</option>";
+	}
+	$output .= '</select> (year in three parts)';
+	$output .= '</form>';
+	$output .= '<div style="text-align: right;"><a href="http://jonradio.com/plugins/jonradio-perpetual-calendar/help" target="_blank">Help and Info</a></div><hr />';
+	return $output;
+}
+
+/**
+ * A.D. or B.C.
+ * 
+ * Returns B.C. for negative years and A.D. otherwise
+ *
+ * @param    int     $century    Year
+ * @return   string              A.D. or B.C.
+ */
+function jr_pc_century( $century ) {
+	if ( $century < 0 ) {
+		return 'B.C.';
+	} else {
+		return 'A.D.';
+	}
+}
+
+/**
+ * Check for signed integer
+ * 
+ * Determines if the parameter is a signed integer, including Strings and Floating Point ending .000
+ *
+ * @param    var     $val        Value to check
+ * @return   bool                TRUE if signed integer; FALSE otherwise
+ */
+function jr_pc_signed_integer( $val ) {
+	if ( is_numeric( $val ) && ( (int)$val == $val ) ) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,69 @@
+=== jonradio Perpetual Calendar ===
+Contributors: jonradio
+Donate link: http://jonradio.com/plugins
+Tags: calendar, weekday
+Requires at least: 3.0
+Tested up to: 3.4
+Stable tag: 0.9
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Provides both a Shortcode and a php Function that each return a message indicating the full name of the day of the week for a given date.
+
+== Description ==
+
+Gives web site visitors a quick way to determine the Day of the Week for any date up to almost seven thousand years in the past or future.  The plugin provides both a [pcal] shortcode and jr_weekday function, providing a choice in how it is implemented.
+
+The term "Perpetual Calendar" normally refers to the display a calendar of any week or month across a broad range of years.  However, it is almost always used to determine the day of the week for a given date, which is what this plugin does.
+
+Once the plugin is successfully installed and activated, adding the Shortcode [pcal] to any WordPress Post or Page will insert a Form that prompts the user to select a Month, Day, and Year from drop-down lists.  Hitting the Display Day of Week button will generate a message above the Form indicating the full name of the day of the week for the given date.  Or an error message for all invalid dates.  A link to additional help is also provided for the site visitor.
+
+To reduce the size of the drop-down lists, the Year is entered in three parts:  (1) A.D. or B.C. and first one or two digits ("century"); (2) second to last digit ("tens"); (3) last digit.
+
+The jr_weekday function is also defined upon plugin activation, so that it can be called in any php code, including templates, within your WordPress environment.  It returns the same message as the [pcal] shortcode; it does NOT directly display through echo or other means.  And it does not display the user input <form>.
+
+If Network is turned on in WordPress, Network Activation of the plugin allows both the plugin and shortcode to be called from all WordPress sites within the Network.  Alternatively, the plugin can also be activated for individual sites within the Network.
+
+Supported dates range from November 25, 4714 B.C. to December 31, 9999 A.D.; the jr_weekday function accepts years larger than 9999, but it has not been tested for accuracy past the year 9999.  Illegal dates, such as February 30 of any year, and the Year Zero (A.D. or B.C.), are detected and an error message is returned in place of the message indicating the weekday.
+
+Multiple uses of the [pcal] Shortcode on the same Page are not supported, and has not been tested.  Likewise, displaying multiple Posts, with [pcal] occuring more than once across all the displayed Posts, has not been tested.  Use at your own risk.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Use "Add Plugin" within the WordPress Admin panel to download and install this plugin from the WordPress.org plugin repository (preferred method).  Or download and unzip this plugin, then upload the `/jonradio-perpetual-calendar/` directory to your WordPress web site's `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Insert the [pcal] shortcode in a WordPress Page or Post; or call the jr_weekday function from any php code, passing the function integer values for Year, Month and Day of Month, and displaying the message returned by the function
+
+== Frequently Asked Questions ==
+
+= Can I use the jr_weekday php function to validate a date? =
+
+Yes, by checking the first three characters of the returned value for "Err".  If there is enough interest, we will create a new plugin with a jr_validate_date function.  Please let us know, including your ideas on how you would like it to work, and how you would use this function.  Note that php has a similar date validation function, but it supports a different range of dates, so please let us know what range of dates you are interested in.
+
+= In a WordPress Network (Multisite) installation, how do I force Perpetual Calendar on only some sites? =
+
+Do not Network Activate this plugin.  Instead, Activate it on each site individually, using the Admin panel for each site, not the Network Admin panel.
+
+= How can I get just the day of the week, without all the other information? =
+
+If there is enough interest, we will add that to a future version.  Please let us know, including your ideas on how you would like it to work, and how you would use this feature.
+
+== Screenshots ==
+
+1. WordPress Page with [pcal] Shortcode when first displayed
+2. WordPress Page with [pcal] Shortcode after a date has been entered and Display Day of Week button has been pushed
+3. WordPress Page with error message displayed because of invalid date ("February 31")
+4. Plugin site page displayed when Help and Info link clicked by site visitor
+
+== Changelog ==
+
+= 0.9 =
+* Make private plugin conform to WordPress plugin repository standards.
+* Add link below ShortCode's form to explanation of the plugin.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Beta version 0.9 had not been tested for WordPress Plugin Repository installation
