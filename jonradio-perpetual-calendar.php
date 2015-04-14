a590245f6a50e6294b9cd96699fe06a4e83d9293
dgewirtz <dgewirtz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 02:24:13 +0000

    Readme
    git-svn-id: https://plugins.svn.wordpress.org/jonradio-perpetual-calendar/trunk@1134099 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jonradio-perpetual-calendar.php
+++ jonradio-perpetual-calendar.php
@@ -4,7 +4,7 @@ Plugin Name: jonradio Perpetual Calendar
 Plugin URI: http://zatzlabs.com/lab-notes/
 Description: Determines Day of the Week for any Date, 7000 years into the past or future, with a Shortcode allowing Web Site Visitors to enter any date and display Day of Week, and a PHP Function accepting Day, Month and Year and returning a String with the Formatted Date and Day of Week.
 Version: 3.0
-Author: dgewirtz
+Author: David Gewirtz
 Author URI: http://zatzlabs.com/lab-notes/
 Text Domain: jonradio-perpetual-calendar
 Domain Path: /languages
